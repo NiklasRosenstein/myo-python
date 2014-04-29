c28d56f4d805c1e0453996ddb95a617a07890f4d
Niklas Rosenstein <rosensteinniklas@gmail.com>
2014-04-29 04:29:53 +0200

    Added myo.tools.macaddr_to_int

--- myo/tools.py
+++ myo/tools.py
@@ -3,6 +3,19 @@
 
 from myo import six
 
+def macaddr_to_int(addr):
+    r""" Converts a string MAC address to an integer. Raises a
+    ValueError if *addr* is not in a MAC address format. """
+
+    addr = addr.replace(':', '')
+    if len(addr) != 12:
+        raise ValueError('not a valid MAC address')
+
+    try:
+        return int(addr, 16)
+    except ValueError:
+        return ValueError('not a valid MAC address')
+
 class ShortcutAccess(object):
     r""" Wrapper for any kind of object to make the access to
     attributes easier. Prefixes all accssed attribute names with
