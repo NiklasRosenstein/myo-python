df6dddd048c9629126a2d12c0de43115d36445c3
Niklas Rosenstein <rosensteinniklas@gmail.com>
2014-04-29 04:56:07 +0200

    Updated __init__.py

--- myo/__init__.py
+++ myo/__init__.py
@@ -1,5 +1,12 @@
 # Copyright (C) 2014  Niklas Rosenstein
 # All rights reserved.
 
-from myo._extern import init
+__author__ = ('Niklas Rosenstein', 'rosensteinniklas@gmail.com')
+__version__ = (0, 1, 0)
+
+# The latest version number of the Myo SDK that this library
+# is compatible with.
+sdk_version = 5
+
+from myo._extern import *
 
