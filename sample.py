432fde28e0ad246971849222a4d4ca24039611d6
Niklas Rosenstein <rosensteinniklas@gmail.com>
2014-04-29 18:05:23 +0200

    Sample application is now Py3 compatible, closes #6

--- sample.py
+++ sample.py
@@ -1,23 +1,22 @@
 # Copyright (C) 2014  Niklas Rosenstein
 # All rights reserved.
 
-import sys
 import time
-import traceback
-
 import myo
 myo.init()
+
+from myo.six import print_
 
 class Listener(myo.DeviceListener):
 
     def on_connect(self, myo, timestamp):
-        print "Connected to Myo"
+        print_("Connected to Myo")
         myo.vibrate('short')
         myo.training_load_profile()
 
     def on_pose(self, myo, timestamp, pose):
         if pose != 'none':
-            print "You just made the", pose.name, "pose!"
+            print_("You just made the", pose.name, "pose!")
 
 def main():
     listener = Listener()
