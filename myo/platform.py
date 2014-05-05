2ceed20735741899f413a47c0471e1e86304d7ac
Niklas Rosenstein <rosensteinniklas@gmail.com>
2014-05-05 16:49:00 +0200

    updated myo.platform module to also detect the platform architecture

--- myo/platform.py
+++ myo/platform.py
@@ -4,17 +4,21 @@
 from __future__ import absolute_import
 
 import os
+import sys
 import platform as _platform
 
 def select():
+    arch = 'x64' if sys.maxsize > (2 ** 32) else 'x86'
     platform = _platform.platform().split('-')[0]
 
     if platform.startswith('Windows'):
-        return 'Windows'
+        result = 'Windows'
     elif platform.startswith('Darwin'):
-        return 'Darwin'
+        result = 'Darwin'
     else:
         raise EnvironmentError('unsupported platform %s' % platform)
 
-platform = select()
+    return result, arch
+
+platform, arch = select()
 
