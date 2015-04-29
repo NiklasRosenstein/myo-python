97c106e67a051b0cffed1eedda6500c47c3cba4d
Niklas Rosenstein <rosensteinniklas@gmail.com>
2015-04-29 18:37:16 +0200

    Updated example to use __future__.print_function

--- examples/hello_myo.py
+++ examples/hello_myo.py
@@ -18,10 +18,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+from __future__ import print_function
+
 import myo
-from myo.lowlevel import pose_t, stream_emg
-from myo.six import print_
 import random
+
+from myo.lowlevel import pose_t, stream_emg
 
 myo.init()
 
@@ -35,12 +37,12 @@ class Listener(myo.DeviceListener):
     # return False from any method to stop the Hub
 
     def on_connect(self, myo, timestamp):
-        print_("Connected to Myo")
+        print("Connected to Myo")
         myo.vibrate('short')
         myo.request_rssi()
 
     def on_rssi(self, myo, timestamp, rssi):
-        print_("RSSI:", rssi)
+        print("RSSI:", rssi)
 
     def on_event(self, event):
         r""" Called before any of the event callbacks. """
@@ -51,24 +53,24 @@ class Listener(myo.DeviceListener):
         the callbacks requested the stop of the Hub. """
 
     def on_pair(self, myo, timestamp):
-        print_('Paired')
-        print_("If you don't see any responses to your movements, try re-running the program or making sure the Myo works with Myo Connect (from Thalmic Labs).")
-        print_("Double tap enables EMG.")
-        print_("Spreading fingers disables EMG.\n")
+        print('Paired')
+        print("If you don't see any responses to your movements, try re-running the program or making sure the Myo works with Myo Connect (from Thalmic Labs).")
+        print("Double tap enables EMG.")
+        print("Spreading fingers disables EMG.\n")
 
     def on_disconnect(self, myo, timestamp):
-        print_('on_disconnect')
+        print('on_disconnect')
 
     def on_pose(self, myo, timestamp, pose):
-        print_('on_pose', pose)
+        print('on_pose', pose)
         if pose == pose_t.double_tap:
-            print_("Enabling EMG")
-            print_("Spreading fingers disables EMG.")
-            print_("=" * 80)
+            print("Enabling EMG")
+            print("Spreading fingers disables EMG.")
+            print("=" * 80)
             myo.set_stream_emg(stream_emg.enabled)
         elif pose == pose_t.fingers_spread:
-            print_("=" * 80)
-            print_("Disabling EMG")
+            print("=" * 80)
+            print("Disabling EMG")
             myo.set_stream_emg(stream_emg.disabled)
 
     def on_orientation_data(self, myo, timestamp, orientation):
@@ -81,23 +83,23 @@ class Listener(myo.DeviceListener):
         show_output('gyroscope', gyroscope)
 
     def on_unlock(self, myo, timestamp):
-        print_('unlocked')
+        print('unlocked')
 
     def on_lock(self, myo, timestamp):
-        print_('locked')
+        print('locked')
 
     def on_sync(self, myo, timestamp, arm, x_direction):
-        print_('synced', arm, x_direction)
+        print('synced', arm, x_direction)
 
     def on_unsync(self, myo, timestamp):
-        print_('unsynced')
+        print('unsynced')
 
     def on_emg(self, myo, timestamp, emg):
         show_output('emg', emg)
 
 def show_output(message, data):
     if random.random() < SHOW_OUTPUT_CHANCE:
-        print_(message + ':' + str(data))
+        print(message + ':' + str(data))
 
 def main():
     hub = myo.Hub()
@@ -110,7 +112,7 @@ def main():
         while hub.running:
             myo.time.sleep(0.2)
     except KeyboardInterrupt:
-        print_("Quitting ...")
+        print("Quitting ...")
         hub.stop(True)
 
 if __name__ == '__main__':
