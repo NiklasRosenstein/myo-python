3202625e0799340f1ee7168b78b5e9803c86f571
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-06-03 17:16:28 +0200

    update setup.py, .gitignore and MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 
 from setuptools import setup
 
+with open('README.md') as fp:
+  readme = fp.read()
+
 with open('requirements.txt') as fp:
   install_requires = fp.readlines()
 
@@ -8,6 +11,8 @@ setup(
   name='myo-python',
   version='1.0.0',
   description='Python bindings for the Thalmic Labs Myo SDK',
+  long_description=readme,
+  long_description_content_type='text/markdown',
   author='Niklas Rosenstein',
   author_email='rosensteinniklas@gmail.com',
   url='https://github.com/NiklasRosenstein/myo-python',
